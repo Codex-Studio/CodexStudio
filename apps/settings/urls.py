3b40b723e6a6f16b98a2a49287e8bff510eaccc3
Toktorov <ktoktorov2005@gmail.com>
2022-11-20 17:21:30 +0600

    service detail, contact

--- apps/settings/urls.py
+++ apps/settings/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path 
-from apps.settings.views import index, portfolio_index, portfolio_detail
+from apps.settings.views import index, portfolio_index, portfolio_detail, services_index, contact
 
 urlpatterns = [
     path('', index, name = "index"),
     path('portfolio/', portfolio_index, name = "portfolio_index"),
     path('portfolio/<int:id>/', portfolio_detail, name = "portfolio_detail"),
+    path('services/', services_index, name = "services_index"),
+    path('contact/', contact, name = "contact")
 ]
--- apps/settings/views.py
+++ apps/settings/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from apps.settings.models import Setting, Portfolio, Partners
+from apps.settings.models import Setting, Portfolio, Partners, Service
 
 # Create your views here.
 def index(request):
     setting = Setting.objects.latest('id')
     partners = Partners.objects.all().order_by('-id')
+    services = Service.objects.all().order_by('-id')
     context = {
         'setting' : setting,
         'partners' : partners,
+        'services' : services,
     }
     return render(request, 'index.html', context)
 
@@ -31,3 +33,19 @@ def portfolio_detail(request, id):
         'portfolio' : portfolio,
     }
     return render(request, 'portfolio_detail.html', context)
+
+def services_index(request):
+    setting = Setting.objects.latest('id')
+    services = Service.objects.all().order_by('-id')
+    context = {
+        'setting' : setting,
+        'services' : services,
+    }
+    return render(request, 'services.html', context)
+
+def contact(request):
+    setting = Setting.objects.latest('id')
+    context = {
+        'setting' : setting,
+    }
+    return render(request, 'contact.html', context)
